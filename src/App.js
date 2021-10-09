229b7f07390b7c73bcf12d39adad58fe35390da3
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-10-09 21:32:09 +0300

    create decorator for popular posts

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+
+function New(props) {
+    return (
+        <div className="wrap-item wrap-item-new">
+            <span className="label">New!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function Popular(props) {
+    return (
+        <div className="wrap-item wrap-item-popular">
+            <span className="label">Popular!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function updateContent(Component) {
+  return function Wrapper(props) {
+    console.log()
+    if (props.views < 100) {
+      return(
+        <New>
+          <Component {...props}/>
+        </New>
+      ) 
+    } 
+    if (props.views > 1000) {
+      return(
+      <Popular>
+        <Component {...props}/>
+      </Popular>
+      ) 
+    } 
+    return(<Component {...props}/>) 
+  }
+}
+
+const UpdateVideo = updateContent(Video);
+const UpdateArticle = updateContent(Article);
+
+function Article(props) {
+    return (
+        <div className="item item-article">
+          {/* eslint-disable-next-line */}
+            <h3><a href="#">{props.title}</a></h3>
+            <p className="views">Прочтений: {props.views}</p>
+        </div>
+    )
+};
+
+function Video(props) {
+    return (
+        <div className="item item-video">
+          {/* eslint-disable-next-line */}
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <p className="views">Просмотров: {props.views}</p>
+        </div>
+    )
+};
+
+function List(props) {
+  /* eslint-disable-next-line */
+    return props.list.map(item => {
+      /* eslint-disable-next-line */
+        switch (item.type) {
+            case 'video':
+                return (
+                    <UpdateVideo {...item} />
+                );
+
+            case 'article':
+                return (
+                    <UpdateArticle {...item} />
+                );
+        }
+    });
+};
+
+export default function App() {
+  /* eslint-disable-next-line */
+    const [list, setList] = useState([
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 50
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 12
+        },
+        {
+            type: 'article',
+            title: 'Невероятные события в неизвестном поселке...',
+            views: 175
+        },
+        {
+            type: 'article',
+            title: 'Секретные данные были раскрыты!',
+            views: 1532
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 4253
+        },
+        {
+            type: 'article',
+            title: 'Кот Бегемот обладает невероятной...',
+            views: 12,
+        },
+    ]);
+
+    return (
+        <List list={list} />
+    );
+}
